d77488a36ef4e8102d68664ce425df921dcfe2d9
janetjk <janetjk@metropolia.fi>
2025-01-16 13:45:45 +0200

    mod2t3

---
+++ Ohjelmisto_1/MOD2_3.py
@@ -0,0 +1,9 @@
+import math
+kanta_str = input(f"mikä on suorakulmion kanta")
+kanta = float(kanta_str)
+korkeus_str = input(f"mikä on suorakulmion korkeus")
+korkeus = float(korkeus_str)
+piiri = (kanta+kanta+korkeus+korkeus)
+pintaala = (kanta*korkeus)
+print (f"suorakulmion piiri on {piiri}  ")
+print (f"suorakulmion pinta-ala on {pintaala}")
