c89e930937822fc86ca68f2530fa72bbb3351d4b
janetjk <janetjk@metropolia.fi>
2025-01-21 15:31:32 +0200

    mod3

---
+++ Ohjelmisto_1/MODUULI3/MOD3_3.py
@@ -0,0 +1,30 @@
+sukuelimet = input("Oletko syntynyt peniksellä vai vaginalla:")
+if sukuelimet == 'penis'or 'peniksellä':
+    hemoglob = float(input("Mikä on hemoglobiiniarvosi?"))
+    if hemoglob<=(134):
+        print ("Hemoglobiinisi on alhainen.")
+    elif hemoglob>=(195):
+        print("Hemoglobiinisi on liian korkea.")
+    elif 134<=hemoglob<195:
+        print("Hemoglobiinisi on normaali.")
+
+elif sukuelimet == 'vagina' or 'vaginalla':
+    hemoglobn = float(input("Mikä on hemoglobiiniarvosi?"))
+    if hemoglobn <= (117):
+        print("Hemoglobiinisi on alhainen.")
+    elif hemoglobn >= (175):
+        print("Hemoglobiinisi on liian korkea.")
+    elif 117 <= hemoglobn < 175:
+        print("Hemoglobiinisi on normaali.")
+
+
+
+
+
+
+
+
+
+#if 15<=ikä<18:
+   # Naisen normaali hemoglobiiniarvo on välillä 117-175 g/l.
+    #Miehen normaali hemoglobiiniarvo on välillä 134-195 g/l.
