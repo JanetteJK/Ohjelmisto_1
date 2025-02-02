5eb091e2c05bc7602f08015d3a7c3c09c233ad78
JanetteJK <janetjk@metropolia.fi>
2025-02-02 22:26:17 +0200

    korjauksia

--- Ohjelmisto_1/MODUULI5/MOD5_2.py
+++ Ohjelmisto_1/MODUULI5/MOD5_2.py
@@ -3,6 +3,6 @@ luku = str(input("Anna ensimmäinen luku tai paina Enter lopettaaksesi:\n"))
 while (luku!= ""):
     luku = int(luku)
     luvut.append(luku)
-    luku = str(input("Anna ensimmäinen luku tai paina Enter lopettaaksesi:\n"))
+    luku = str(input("Anna seuraava luku tai paina Enter lopettaaksesi:\n"))
 luvut.sort(reverse=True)
-print (luvut[0:6])
+print (luvut[0:5])
